894e96cadc05732df6abd00baa072dc98cae2ec9
Michelle Dadighat <michelle.dadighat@noirlab.edu>
2023-09-12 14:26:03 -0700

    test deployment to ECS

--- api/core/__init__.py
+++ api/core/__init__.py
@@ -16,7 +16,8 @@ limiter = Limiter(
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://{}:{}@{}:{}/{}".format(
     db_login[0], db_login[1], db_login[2], db_login[3], db_login[4]) 
-    #"postgres", "sat123", "localhost", "5432", "postgres")
+    #test
+    #"postgres", "sat123", "localhost", "5432", "postgres") 
 db = SQLAlchemy(app)
 
 from core import routes
