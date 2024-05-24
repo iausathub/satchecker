6c7efbe9ce7d876d0b19af3b6beca8a2e7f1fbea
Jason Kalawe <jason.kalawe@noirlab.edu>
2024-05-24 09:27:26 -1000

    get secrets from environmental variables

--- data/retrieve_tle_aws.py
+++ data/retrieve_tle_aws.py
@@ -250,6 +250,17 @@ def insert_records(
 
 
 def get_db_login():
+
+    if os.environ.get("DB_HOST") is not None:
+        username, password, host, port, dbname = (
+            os.environ.get("DB_USERNAME"),
+            os.environ.get("DB_PASSWORD"),
+            os.environ.get("DB_HOST"),
+            os.environ.get("DB_PORT"),
+            os.environ.get("DB_NAME"),
+        )
+        return [username, password, host, port, dbname]
+
     secret_name = "satchecker-prod-db-cred"  # noqa: S105
 
     secrets = get_secret(secret_name)
