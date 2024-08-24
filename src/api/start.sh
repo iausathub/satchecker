61bae0f8abcd7176e2f813ec8b6e0179080c5d24
Michelle Dadighat <michelle.dadighat@noirlab.edu>
2024-08-24 14:34:26 -0700

    Update path to app for gunicorn in start.sh

--- src/api/start.sh
+++ src/api/start.sh
@@ -9,4 +9,4 @@ celery -A api.satchecker.celery worker --loglevel INFO &
 
 
 # Start the Flask server in the foreground
-gunicorn --workers=3 --timeout 120 -b 0.0.0.0:5000 --access-logfile - satchecker:app
+gunicorn --workers=3 --timeout 120 -b 0.0.0.0:5000 --access-logfile - api.satchecker:app
