daabe9325f49aaec20e7bd691d10cd40959dca17
Michelle Dadighat <michelle.dadighat@noirlab.edu>
2024-08-26 11:45:04 -0700

    Test changes to resolve namespace error in deployed docker container

--- src/api/start.sh
+++ src/api/start.sh
@@ -7,6 +7,6 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)/src"
 # Start the Celery worker in the background
 celery -A api.satchecker.celery worker --loglevel INFO &
 
-
+echo $PYTHONPATH
 # Start the Flask server in the foreground
-gunicorn --workers=3 --timeout 120 -b 0.0.0.0:5000 --access-logfile - api.satchecker:app
+exec gunicorn --workers=3 --timeout 120 -b 0.0.0.0:5000 --access-logfile - api.satchecker:app
