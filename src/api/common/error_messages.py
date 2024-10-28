f0e4bf7caae660ecce8d33ab973fec786be7aba0
Michelle Dadighat <michelle.dadighat@noirlab.edu>
2024-10-28 09:39:15 -0700

    Add ability to get TLE list as .zip file with CSV

--- src/api/common/error_messages.py
+++ src/api/common/error_messages.py
@@ -18,6 +18,7 @@ INVALID_PARAMETER = "Error: Invalid parameter format"
 INVALID_JD = "Error: Invalid Julian Date"
 INVALID_SOURCE = "Error: Invalid data source"
 INVALID_TLE = "Error: Invalid TLE format"
+INVALID_FORMAT = "Error: Invalid result format"
 NO_TLE_FOUND = "Error: No TLE found"
 TOO_MANY_RESULTS = (
     "Error: Too many results to return (maximum of 1000 in a single request)"
--- src/api/services/validation_service.py
+++ src/api/services/validation_service.py
@@ -188,6 +188,12 @@ def validate_parameters(
         except Exception as e:
             raise ValidationError(500, error_messages.INVALID_JD, e) from e
 
+    if "format" in parameters.keys():
+        parameters["format"] = parameters["format"].lower()
+
+        if parameters["format"] not in ["json", "zip"]:
+            raise ValidationError(500, error_messages.INVALID_FORMAT)
+
     return parameters
 
 
