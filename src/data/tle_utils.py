0170445e6126debcc964d93971127d6c21a2cd01
Michelle Dadighat <michelle.dadighat@noirlab.edu>
2025-01-14 22:37:59 -0800

    Revert "Change date update script to insert new records into tle_partitioned; add separate function to support adding archive data"
    This reverts commit de8858a6adb247f6e61ff388a40029247c217b4d.

--- src/data/tle_utils.py
+++ src/data/tle_utils.py
@@ -3,7 +3,6 @@ import logging
 
 import requests
 from connections import get_spacetrack_login
-from psycopg2.extras import execute_values
 from skyfield.api import EarthSatellite, load
 
 
@@ -44,7 +43,6 @@ def insert_records(
         str(satellite.model.satnum),
         name,
     )
-
     satellite_insert_query = """ WITH e AS(
     INSERT INTO satellites (SAT_NUMBER, SAT_NAME, CONSTELLATION,
     DATE_ADDED, DATE_MODIFIED, RCS_SIZE, LAUNCH_DATE, DECAY_DATE, OBJECT_ID,
@@ -95,7 +93,7 @@ def insert_records(
 
     # add TLE to database
     tle_insert_query = """
-    INSERT INTO tle_partitioned (SAT_ID, DATE_COLLECTED, TLE_LINE1, TLE_LINE2, \
+    INSERT INTO tle (SAT_ID, DATE_COLLECTED, TLE_LINE1, TLE_LINE2, \
         EPOCH, IS_SUPPLEMENTAL, DATA_SOURCE) VALUES (%s,%s,%s,%s,%s,%s,%s)
         ON CONFLICT (SAT_ID, EPOCH, DATA_SOURCE) DO NOTHING;"""
     record_to_insert = (
@@ -111,126 +109,9 @@ def insert_records(
     cursor.execute(tle_insert_query, record_to_insert)
 
 
-def insert_archival_tles(
-    records, timescale, cursor, connection, constellation, is_supplemental, source
-):
-    batch_size = 1000
-    tle_values = []
-    for i, record in enumerate(records):
-
-        name = record["OBJECT_NAME"]
-        tle_line_1 = record["TLE_LINE1"]
-        tle_line_2 = record["TLE_LINE2"]
-
-        rcs_size = record.get("RCS_SIZE", None)
-        launch_date = record.get("LAUNCH_DATE", None)
-        decay_date = record.get("DECAY_DATE", None)
-        object_id = record.get("OBJECT_ID", None)
-        object_type = record.get("OBJECT_TYPE", None)
-
-        satellite = EarthSatellite(tle_line_1, tle_line_2, name=name, ts=timescale)
-        current_date_time = datetime.datetime.now(datetime.timezone.utc)
-
-        check_query = """
-         SELECT EXISTS (
-            SELECT 1 FROM satellites
-            WHERE sat_number = %s OR sat_name = %s
-            ) as exists_either;
-        """
-        cursor.execute(check_query, (satellite.model.satnum, name))
-        exists_either = cursor.fetchone()[0]
-        is_current_number = (
-            not exists_either
-        )  # can be true only if completely new satellite
-
-        sat_to_insert = (
-            satellite.model.satnum,
-            name,
-            constellation,
-            current_date_time,
-            current_date_time,
-            rcs_size,
-            launch_date,
-            decay_date,
-            object_id,
-            object_type,
-            is_current_number,
-            str(satellite.model.satnum),
-            name,
-        )
-
-        satellite_insert_query = """ WITH e AS(
-        INSERT INTO satellites (SAT_NUMBER, SAT_NAME, CONSTELLATION,
-        DATE_ADDED, DATE_MODIFIED, RCS_SIZE, LAUNCH_DATE, DECAY_DATE, OBJECT_ID,
-        OBJECT_TYPE, HAS_CURRENT_SAT_NUMBER)
-        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-        ON CONFLICT (SAT_NUMBER, SAT_NAME) DO NOTHING
-        RETURNING id)
-        SELECT * FROM e
-        UNION ALL
-        (SELECT id FROM satellites WHERE SAT_NUMBER=%s AND SAT_NAME=%s);"""
-        cursor.execute(satellite_insert_query, sat_to_insert)
-        sat_id = cursor.fetchone()[0]
-
-        if cursor.rowcount == 0:
-            log_time = datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%d %H:%M:%S")
-            logging.info(f"{log_time}\tnew satellite: {satellite.model.satnum}")
-
-        # make sure this satellite gets committed to the database
-        connection.commit()
-
-        tle_values.append(
-            (
-                sat_id,
-                current_date_time,
-                tle_line_1,
-                tle_line_2,
-                satellite.epoch.utc_datetime(),
-                is_supplemental,
-                source,
-            )
-        )
-
-        if len(tle_values) >= batch_size:
-            execute_values(
-                cursor,
-                """
-                INSERT INTO tle_partitioned (sat_id, date_collected,
-                tle_line1, tle_line2,cepoch, is_supplemental, data_source)
-                VALUES %s
-                ON CONFLICT (sat_id, epoch, data_source) DO NOTHING
-                """,
-                tle_values,
-            )
-            tle_values = []
-            print(f"TLEs added: {i}")
-            connection.commit()
-
-    if len(tle_values) > 0:
-        execute_values(
-            cursor,
-            """
-            INSERT INTO tle_partitioned (sat_id, date_collected, tle_line1, tle_line2,
-                            epoch, is_supplemental, data_source)
-            VALUES %s
-            ON CONFLICT (sat_id, epoch, data_source) DO NOTHING
-            """,
-            tle_values,
-        )
-        print(f"Final TLEs added: {len(tle_values)}")
-        connection.commit()
-
-
 # Parse TLE list and add entries to database if they don't exist
 def add_tle_list_to_db(
-    tle,
-    constellation,
-    cursor,
-    connection,
-    is_supplemental,
-    source,
-    is_current_number,
-    is_archival,
+    tle, constellation, cursor, is_supplemental, source, is_current_number
 ):
     tle_count = 0
     if source == "celestrak":
@@ -260,29 +141,17 @@ def add_tle_list_to_db(
     elif source == "spacetrack":
         tle_json = tle.json()
         ts = load.timescale()
-        if is_archival:
-            insert_archival_tles(
-                tle_json,
+        for record in tle_json:
+            insert_records(
+                record,
                 ts,
                 cursor,
-                connection,
                 constellation,
                 is_supplemental,
                 source,
+                is_current_number,
             )
-            tle_count += len(tle_json)
-        else:
-            for record in tle_json:
-                insert_records(
-                    record,
-                    ts,
-                    cursor,
-                    constellation,
-                    is_supplemental,
-                    source,
-                    is_current_number,
-                )
-                tle_count += 1
+            tle_count += 1
 
     log_time = datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%d %H:%M:%S")
     logging.info(f"{log_time}\tTLE source: {source}")
