11fdd0679c49092697085c9bd416ff6e787ef6b7
Michelle Dadighat <michelle.dadighat@noirlab.edu>
2025-01-22 17:07:41 -0800

    Change pythonpath

--- src/api/start.sh
+++ src/api/start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-export PYTHONPATH="/usr/src/app/api:${PYTHONPATH}:$(pwd)/src"
+# Set PYTHONPATH to include the parent directory of api
+cd /usr/src/app/api
+export PYTHONPATH="/usr/src/app/api"
 
 # Run database migrations
-cd /usr/src/app/api
 alembic -c migrations/alembic.ini upgrade head
 
 # Start the Celery worker in the background
 celery -A api.satchecker.celery worker --loglevel INFO &
 
-echo $PYTHONPATH
 # Start the Flask server in the foreground
 exec gunicorn --workers=3 --timeout 120 -b 0.0.0.0:5000 --access-logfile - api.satchecker:app
