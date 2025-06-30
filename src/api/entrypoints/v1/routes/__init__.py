778dd0a8d3e75966eb21b811e61b0cd00a8bcdbb
Michelle Dadighat <michelle.dadighat@noirlab.edu>
2025-06-30 11:48:26 -0700

    Rename Towncrier fragments and update version to 1.4.0

--- src/api/entrypoints/v1/routes/__init__.py
+++ src/api/entrypoints/v1/routes/__init__.py
@@ -4,7 +4,7 @@ api_v1 = Blueprint("api_v1", __name__)
 api_main = Blueprint("api_main", __name__)
 
 api_source = "IAU CPS SatChecker"
-api_version = "1.3.1"
+api_version = "1.4.0"
 
 # Import all route modules to register the routes with the blueprints
 # This ensures all routes are registered when the blueprints are imported elsewhere
