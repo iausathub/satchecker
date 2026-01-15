f929bb1b116c4c5edf6a03e9dee809a18c96f74a
Michelle Dadighat <michelle.dadighat@noirlab.edu>
2026-01-15 13:19:09 -0800

    Update start.sh to modify server and worker settings
    Removed the background start of the Celery worker and changed the Gunicorn worker class to gthread.

--- src/api/start.sh
+++ src/api/start.sh
@@ -7,8 +7,5 @@ export PYTHONPATH="/usr/src/app"
 # Run database migrations
 alembic -c migrations/alembic.ini upgrade head
 
-# Start the Celery worker in the background
-celery -A api.satchecker.celery worker --loglevel INFO &
-
 # Start the Flask server in the foreground
-exec gunicorn --workers=4 --threads=4 --timeout 300 -b 0.0.0.0:5000 --access-logfile - --keep-alive 120 api.satchecker:app
+exec gunicorn --workers=4 --threads=4 --worker-class=gthread --timeout 300 -b 0.0.0.0:5000 --access-logfile - --keep-alive 120 api.satchecker:app
